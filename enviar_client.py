4e651181c7f33d275b9367268b8bf72a1a0c423b
marcos <marcosperezsouto@gmail.com>
2019-12-04 01:37:34 -0800

    Añadir archivo a git
    Añadir a git el archivo enviar_client.py

---
+++ enviar_client.py
@@ -0,0 +1,14 @@
+
+import paho.mqtt.client as mqtt
+import time
+           
+broker_address="10.0.0.153"
+client = mqtt.Client('marcos')
+client.connect(broker_address)
+
+topic = "prueba"
+payload = "hola"
+client.publish(topic, payload)
+
+
+
