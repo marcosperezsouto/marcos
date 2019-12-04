e3c29bf8136375e014e339c51e5104f55a2545e2
marcos <marcosperezsouto@gmail.com>
2019-12-04 01:42:50 -0800

    Añadir fichero recibir_client.py

---
+++ recibir_client.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt 
+
+broker_address = "localhost"
+broker_port = 1883
+topic = "prueba"
+
+def on_connect(client, userdata, flags, rc):
+    print("on connect"+str(rc))
+    try:
+        client.subscribe(topic)
+        print("subscribed")
+    except:
+        print("exception")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(broker_address, broker_port, 60)
+
+client.loop_forever()
