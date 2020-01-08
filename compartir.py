a95b65c53a659387fb64428ac3426eb578a65274
marcos <marcosperezsouto@gmail.com>
2020-01-08 18:38:36 +0100

    compartir archivos servidor web
    compartir archivos

---
+++ compartir.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# desde terminal (python3 -m http.server 8000)
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print ("serving at port", PORT)
+    httpd.serve_forever()
