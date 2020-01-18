f1b8973a93b88b9dcdb669d77d6743c5974e325f
marcos <marcosperezsouto@gmail.com>
2020-01-18 19:06:28 +0100

    Ampliar codigo lectura 2 contadores
    Restructurar codigo para la lectura de 2 contadores pin 8 y 9

--- Leer_datos_envio_client.py
+++ Leer_datos_envio_client.py
@@ -3,32 +3,54 @@ import RPi.GPIO as GPIO
 import time
 
 payload = 0
-litros_pulso = 0
+litros_italia = 0
+litros_camara2 = 0
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    Pulso_cont_1 = GPIO.input(24)
-    P_C_1 = not Pulso_cont_1
-        
-    if P_C_1:
-        litros_pulso = litros_pulso + 50
-        payload = litros_pulso
+    Pulso_24 = GPIO.input(24)
+    P_italia = not Pulso_24
+    Pulso_23 = GPIO.input(23)
+    P_camara2 = not Pulso_23        
+
+    if P_italia:
+        litros_italia = litros_italia + 50
         print(time.strftime("%d/%m/%Y-%H:%M"))
-        print(payload, 'litros')
+        print(litros_italia, 'litros italia')
         
+    if P_camara2:
+        litros_camara2 = litros_camara2 + 250
+        print(time.strftime("%d/%m/%Y-%H:%M"))
+        print(litros_camara2, 'litros camara2')
+
     minuto = time.strftime("%M:%S")
+
     if minuto in('00:00', '15:00', '30:00', '45:00'):
-        topic = "prueba"
-        broker_address="localhost"
-        client = mqtt.Client('cefrico')
-        client.connect(broker_address)
-        print('Datos enviados')
-        client.publish(topic, payload)
-        payload = 0
-        litros_pulso = 0
-        time.sleep(1)
+        if litros_italia != 0:    
+            topic = "italia"
+            payload = litros_italia
+            broker_address="localhost"
+            client = mqtt.Client('cefrico')
+            client.connect(broker_address)
+            print('Datos enviados')
+            client.publish(topic, payload)
+            payload = 0
+            litros_italia = 0
+
+        if litros_camara2 != 0:
+            topic = "camara2"
+            payload = litros_camara2
+            broker_address="localhost"
+            client = mqtt.Client('cefrico')
+            client.connect(broker_address)
+            print('Datos enviados')
+            client.publish(topic, payload)
+            payload = 0
+            litros_camara2 = 0
+
     time.sleep(0.3)
     
 
