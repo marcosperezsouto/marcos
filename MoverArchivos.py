7d98e4a8bc801c8ccab66287bf35291b487c51d0
marcos <marcosperezsouto@gmail.com>
2020-02-12 11:08:20 +0100

    mover/copiar archivos 12-2-2020
    reoedenar codigo

--- MoverArchivos.py
+++ MoverArchivos.py
@@ -11,7 +11,7 @@ while True:
 
     hora = time.strftime("%H:%M")
 
-    if hora == '22:30':
+    if hora == '06:50':
 
         ruta = os.getcwd() + os.sep
         origen = "/home/marcos1984/Escritorio/italia.csv"
@@ -39,13 +39,54 @@ while True:
 
         dia = time.strftime("%d-%m-%Y")
 
-        ubicacion = "/home/marcos1984/Documentos/italiaooooo"
+        ubicacion = "/home/marcos1984/Documentos/italia"
         extension = ".csv"
 
         nombre = ubicacion + dia + extension
         print(nombre)
 
         archivo = "/home/marcos1984/Documentos/italia.csv"
+        nombre_nuevo = nombre
+
+        os.rename(archivo, nombre_nuevo)
+
+    time.sleep(50)
+
+    if hora == '06:52':
+
+        ruta = os.getcwd() + os.sep
+        origen = "/home/marcos1984/Escritorio/camara2.csv"
+        destino = "/home/marcos1984/Documentos/camara2.csv"
+
+        if os.path.exists(origen):
+            with open(origen, 'rb') as forigen:
+                with open(destino, 'wb') as fdestino:
+                    shutil.copyfileobj(forigen, fdestino)
+                    print("Archivo copiado")
+
+
+        ruta = os.getcwd() + os.sep
+        origen = "/home/marcos1984/Downloads/camara2.csv"
+        destino = "/home/marcos1984/Escritorio/camara2.csv"
+
+        if os.path.exists(origen):
+            with open(origen, 'rb') as forigen:
+                with open(destino, 'wb') as fdestino:
+                    shutil.copyfileobj(forigen, fdestino)
+                    print("Archivo copiado")
+
+
+# Para renombrar un archivo en Python 3 sólo debes usar la función rename del módulo.
+
+        dia = time.strftime("%d-%m-%Y")
+
+        ubicacion = "/home/marcos1984/Documentos/camara2"
+        extension = ".csv"
+
+        nombre = ubicacion + dia + extension
+        print(nombre)
+
+        archivo = "/home/marcos1984/Documentos/camara2.csv"
         nombre_nuevo = nombre
 
         os.rename(archivo, nombre_nuevo)
